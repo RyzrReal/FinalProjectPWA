6ee1271b0c7aa9007efe4f3333bf41a22ee6b003
RyzrReal <136688110+RyzrReal@users.noreply.github.com>
2026-06-10 04:30:50 +0000

    commit

--- server.py
+++ server.py
@@ -13,4 +13,13 @@ def serve_static(path):
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return "test :)"
+   #return render_template('./index.html')
+
+@app.route('/<path:path>')
+def all_routes(path):
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run()
