31e0a847ef0bc1e20b25d8776755ec2c476e209c
RyzrReal <136688110+RyzrReal@users.noreply.github.com>
2026-06-11 09:57:42 +0000

    Finished Login Page

--- server.py
+++ server.py
@@ -18,10 +18,10 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
 
     def set_password(self, password):     
-        self.password = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
@@ -32,29 +32,55 @@ port = int(os.environ.get("PORT", 5000))
 def serve_static(path):
     return send_from_directory('static', path)
 
+@app.route('/')
+def home():
+    if 'username' in session:
+        return render_template('index.html')
+    return render_template('login.html')
+
 
 @app.route('/<path:path>')
 def all_routes(path):
     return redirect('/')
 
-@app.route('/')
-def login():
-        if 'username' in session:
-            return redirect(url_for('dashboard'))
-        return render_template('login.html')
+
 
 #login
 @app.route('/login', methods=['POST'])
 def login():
     #collects info from form and runs against db
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form["username"]
+    password = request.form["password"]
     user = User.query.filter_by(username=username).first()
     if user and user.check_password(password):
         session['username'] = username
+        return redirect(url_for('home'))
     else:
         return render_template ('login.html')
 
+#register
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form["username"]
+    password = request.form["password"]
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return render_template("login.html", error="User Already Has An Account")
+    else:
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['username'] = username
+        return redirect(url_for('login'))
+
+#dashboard
+
+#logout
+@app.route("/logout")
+def logout():
+    session.pop('username',None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     with app.app_context():
