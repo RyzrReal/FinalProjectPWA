a9065d7de90f7dfe41b2e12a7371049e395b3381
RyzrReal <136688110+RyzrReal@users.noreply.github.com>
2026-06-17 03:45:16 +0000

    CodeComments for server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, send_from_directory, render_template, redirect, url_for, flash, session, g
 from flask import request
 from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 from cryptography.fernet import Fernet
@@ -18,16 +19,16 @@ app = Flask(__name__)
 
 #Db setup
 
-app.secret_key = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
+app.secret_key = secrets.token_hex(32) #makes a key of a scramble of characters.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db' #configures database name and location
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) #creates a database linked to the app.
 
 #database model
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25), unique=True, nullable=False)
-    password_hash = db.Column(db.String(150), nullable=False)
+    id = db.Column(db.Integer, primary_key=True) #creates an id for each user (first person to login would have 1, then next would be 2, so on and so forth and what have you)
+    username = db.Column(db.String(25), unique=True, nullable=False) #creates a username coloum, sets the max characters a username can be (25)
+    password_hash = db.Column(db.String(150), nullable=False) #creates a coloum where the hashed password is stored.
 
     def set_password(self, password):     
         self.password_hash = generate_password_hash(password) #this stores the password as a scramble of letters instead of the actual password
@@ -47,7 +48,7 @@ def home():
         return render_template('index.html')
     return render_template('login.html')
 
-
+#sets that files are allowed and how they are processesed
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -59,6 +60,9 @@ def uploadteach():
     if request.method == "POST":
         conn = sqlite3.connect("database/importdteach.db")
         cursor = conn.cursor()
+
+        #these directories link the html upload fields with the 
+        # faculty coloum of the table, so that the teacher name and course taught matches properly.
 
         teacher_files = {
             "english":"English",
@@ -73,6 +77,7 @@ def uploadteach():
             "adminsupp":"Student Administration and Support"
         }
 
+    #matches the car png foer each faculty with the correct faculty on the table.
         teacher_car = {
             "English" : "englishcar.png",
             "Mathematics" : "mathscar.png",
@@ -86,30 +91,35 @@ def uploadteach():
             "Student Administration and Support" : "saascar.png"
         }
 
+        #loops through all of the uploaded files
         for field_name, faculty in teacher_files.items():
-            file = request.files.get(field_name)
+            file = request.files.get(field_name) #gets the files that were uploaded (if an english file is uploaded,
+            #the field_name would be english)
 
-            if file and file.filename != "":
-                for line in file:
-                    line = line.decode("utf-8").strip()
+            if file and file.filename != "": #checks if a file exsists and isnt left empty in the import
+                for line in file: #reads each line in the file
+                    line = line.decode("utf-8").strip() #uploaded files are read as raw data, the strip function removes spaces and newlines
+                    #so its just read as a line of info
 
                     if line == "":
-                        continue
+                        continue#if there is a blank like, it is ignored
 
-                    parts = line.split(",")
+                    parts = line.split(",") #a name and course (Tim, Metal) becomes ("Tim", "Metal") as they are split by a comma in a string
 
-                    name = parts[0].strip()
-                    course_taught = parts[1].strip() if len(parts) > 1 else ""
-                    carcolour = teacher_car[faculty]
+                    name = parts[0].strip() #gets first value in the list
+                    course_taught = parts[1].strip() if len(parts) > 1 else "" #if a course is added it lists them, otherwise it leaves it blank only importing name
+                    carcolour = teacher_car[faculty] #matches the car with the faculty
 
-
+                    #imports all stripped values into the database for use
                     cursor.execute("""
                         insert into importdteach (name, faculty, course_taught, carcolour, priority)
                         values (?,?,?,?,?) """, (name, faculty, course_taught, carcolour, 1))
 
+        #makes changes permanant and closes
         conn.commit()
         conn.close()
 
+    #returns and redirects to the upload page
         return redirect(url_for("uploadteach"))
     return render_template("uploadteach.html")        
 
@@ -183,6 +193,7 @@ def listofteach():
     data = cursor.fetchall()
     conn.close()
 
+
     return render_template("listofteach.html", all_data=data, current_sort = sort)
 
 def get_db():
@@ -194,11 +205,18 @@ def get_db():
     conn.close()
     return all_data
 
+@app.route("/setparkingnum", methods=["POST"]) 
+def spacenum(): 
+    spaceamout = int(request.form["spacenum"]) #recieves the number that is input in the form
+    session["spaceamout"] = spaceamout #makes the value that was input and the value that will be used the same
+    session.pop("roster_made", None) #removes the prior roster so that a new one can be made
+    return redirect(url_for("make_roster")) #runs the roster make func
 
 def biweeklyrostamakea():
     conn = sqlite3.connect("database/importdteach.db") 
     conn.row_factory = sqlite3.Row #allows the rows in a db to be accessed
     cursor = conn.cursor()
+    spaceamout = session.get("spaceamout", 30)
 
     #list of all days in the biweekyly cycle
     days = ["MonA", "TuesA", "WedA", "ThurA", "FriA", "MonB", 
@@ -210,20 +228,20 @@ def biweeklyrostamakea():
         cursor.execute("select count(*) from importdteach where priority = 1") #if all teachers have priority 0, it resets them all to 1
         available = cursor.fetchone()[0] #stores number of avaliable teachers
 
-        if available < 30:
+        if available < spaceamout:
             cursor.execute("update importdteach set priority = 1") #if no teachers have 1 prio but the roster doesnt have enough to be filled, it resets them all
         
         
         #selects from teacher db
         #only chooses teachers who have priority of 1
         #gets the teachers in a random order
-        #limits to 30 selections
+        #limits to amount chosen by user selections
         cursor.execute("""
             select * from importdteach  
             where priority = 1
             order by random()
-            limit 30
-        """)
+            limit ?
+        """, (spaceamout,))
         chosenteach = cursor.fetchall()
 
         #save chosen teachers into  w important values and such
@@ -251,21 +269,22 @@ def biweeklyrostamakea():
     conn.commit()
     conn.close()
 
-@app.route("/make_roster")
+@app.route("/make_roster") #when a user visits this or it is triggered, it runs the make_roster function
 def make_roster():
-    if session.get("roster_made"):
-        return redirect(url_for("roster"))
+    if session.get("roster_made"): #Checks if a roster has been made
+        return redirect(url_for("roster")) #returns said roster so that i can be viewed
     
     biweeklyrostamakea()
-    session["roster_made"] = True
+    session["roster_made"] = True #sets as true so that multiple rosters cannot be made (it removes the button on the html page)
 
     return redirect(url_for("roster"))
 
 
-def create_roster_tablse():
+def create_roster_tablse(): 
     conn = sqlite3.connect("database/importdteach.db")
     cursor = conn.cursor()
 
+    #creates a table with the data from the database to be used in the roster system, storing the day and all impoirtatnt teachert info
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS roster (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -281,16 +300,17 @@ def create_roster_tablse():
     conn.commit()
     conn.close()
 
-create_roster_tablse()
+create_roster_tablse() #runs the function after its defined
 
 @app.route("/roster")
 def roster():
-    day = request.args.get("day", "MonA")
+    day = request.args.get("day", "MonA") #automatically shows mondayA as the first day.
 
     conn = sqlite3.connect("database/importdteach.db")
-    conn.row_factory = sqlite3.Row
+    conn.row_factory = sqlite3.Row #allows the data to be acsessed by the value, like the name and faculty from that row
     cursor = conn.cursor()
 
+    #gets all from the roster table where the day is whatever has been selected
     cursor.execute("""
         SELECT *
         FROM roster
@@ -317,4 +337,3 @@ if __name__ == '__main__':
         db.create_all()
         create_roster_tablse()
     app.run()
-
